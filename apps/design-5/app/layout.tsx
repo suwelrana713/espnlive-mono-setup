ea1b0c88dfa0d766965fe80d59aef2fc1b64e996
ns nasim <subhoislam624@gmail.com>
2026-09-15 16:02:00 +0600

    refactor: Update Google Analytics integration to GA4

--- apps/design-5/app/layout.tsx
+++ apps/design-5/app/layout.tsx
@@ -184,10 +184,15 @@ export default function RootLayout({
         </Providers>
         <ClickGatedAds />
         <Script
-          id="gtm-script"
+          id="ga4-src"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-S228HEQMKH"
+        />
+        <Script
+          id="ga4-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P6B38HG3');`,
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-S228HEQMKH');`,
           }}
         />
       </body>
